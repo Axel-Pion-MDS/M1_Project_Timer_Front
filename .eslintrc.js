6f386ddc002ea3a97f9463e99a48880c7c389c93
Lucas Malatchoumy <malatchoumylucas.com>
2022-11-30 00:56:40 +0100

    fix: configure correctly eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,27 @@ module.exports = {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
   },
-  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier', 'plugin:vue/recommended'],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    "vue/first-attribute-linebreak": ["error", {
+      "singleline": "beside",
+      "multiline": "beside"
+    }],
+    "vue/max-len": ["error", {
+      "code": 120,
+      "template": 120,
+      "tabWidth": 2,
+      "ignoreComments": false,
+      "ignoreUrls": false,
+      "ignoreStrings": false,
+  }],
+  'comma-dangle': ['error', 'only-multiline'],
+  'no-multiple-empty-lines': 'warn',
+  'space-before-function-paren': 'off',
+  'vue/multi-word-component-names': 'off',
+  'vue/no-v-text-v-html-on-component': 'off',
+  'no-use-before-define': 'warn',
+  },
 }
