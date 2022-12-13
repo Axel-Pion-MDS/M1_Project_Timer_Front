03012f067e5a6842023dea50cdb7c9022ccc5526
Lucas Malatchoumy <malatchoumylucas.com>
2022-12-13 02:04:56 +0100

    feat: create login component

---
+++ components/Login.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-form ref="formulaire" v-model="valid">
+    <v-container>
+      <v-row class="d-flex flex-column">
+        <v-col
+          cols="12"
+        >
+          <v-text-field
+            v-model="form.email"
+            :rules="[rules.required(), rules.isEmail()]"
+            label="E-mail"
+            outlined
+            required
+          />
+        </v-col>
+        <v-col
+          cols="12"
+        >
+          <v-text-field
+            v-model="form.password"
+            type="password"
+            :rules="[rules.required()]"
+            label="Password"
+            outlined
+            required
+          />
+        </v-col>
+        <v-btn
+          class="black--text"
+          :disabled="!valid"
+          color="primary"
+          @click="login"
+        >
+          Login
+        </v-btn>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+<script>
+import { rules } from '~/utils/formValidation'
+export default {
+  data: () => ({
+    rules,
+    valid: true,
+    form: {
+      email: '',
+      password: ''
+    }
+  }),
+  methods: {
+    test() {
+      alert(this.form.email)
+    },
+    validate() {
+      this.$refs.formulaire.validate()
+    },
+    reset () {
+      this.$refs.formulaire.reset()
+    },
+    resetValidation () {
+      this.$refs.formulaire.resetValidation()
+    },
+    login() {
+      this.$store.dispatch('user/login', this.form)
+    }
+  }
+}
+</script>
