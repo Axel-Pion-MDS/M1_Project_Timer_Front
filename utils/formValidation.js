bfee536571b259d717fd238f12b64756d9abc628
Lucas Malatchoumy <malatchoumylucas.com>
2022-12-30 00:40:07 +0100

    refactor: change error message

--- utils/formValidation.js
+++ utils/formValidation.js
@@ -1,7 +1,7 @@
 const isEmpty = value => value === null || value === undefined || value === ''
 
 const isEmail =
-  (msg = ('errors.not_an_email')) =>
+  (msg = ("it's not an email")) =>
     email =>
       // eslint-disable-next-line vue/max-len
       /^(([^<>()[\]\\.,;:\s@"\\']+(\.[^<>()[\]\\.,;:\s@"\\']+)*)|("[^"\\']+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-zA-Z\d\\-]+\.)+[a-zA-Z]{2,}))$/.test(
@@ -9,7 +9,7 @@ const isEmail =
       ) || msg
 
 const required =
-  (msg = ('errors.required')) =>
+  (msg = ('required')) =>
     v =>
       !isEmpty(v) || msg
 
