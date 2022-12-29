b0d43ec7f8c65f15d9864da9e2127595c027b7c4
Lucas Malatchoumy <malatchoumylucas.com>
2022-12-30 00:36:45 +0100

    feat: create signup link if user don't have account

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,9 +1,23 @@
 <template>
-  <div><Login /></div>
+  <div>
+    <h1 class="text-center">
+      Welcome back!
+    </h1>
+    <Login />
+    <p class="text-center">
+      You don't have any account?
+      <nuxt-link to="/auth/signup">
+        Create one!
+      </nuxt-link>
+    </p>
+  </div>
 </template>
 <script>
+import Login from '~/components/Auth/Login.vue'
+
 export default {
   name: 'LoginPage',
+  components: { Login },
   layout: 'auth',
 }
 </script>
