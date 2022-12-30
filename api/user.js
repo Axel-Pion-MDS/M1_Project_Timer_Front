5c3506513d03a4a0aac0a70243cd3d91bd86547e
Lucas Malatchoumy <malatchoumylucas.com>
2022-12-30 21:59:44 +0100

    feat: get user info on default layout

--- api/user.js
+++ api/user.js
@@ -5,5 +5,13 @@ export default axios => ({
   },
   signup(form) {
     return axios.post('/user/add', form)
+  },
+  get_user(token) {
+    return axios.get(`/user/${token.id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('JWT')}`
+      }
+    }
+    )
   }
 })
