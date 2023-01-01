912036c14758dd4ffaeb1011b4b2557ccf60ccee
elton doisy <elton.doisy@gmail.com>
2023-01-01 16:11:46 +0100

    feat: task store

--- store/task.js
+++ store/task.js
@@ -1,35 +1,61 @@
+import axios from 'axios'
+
 export const state = () => ({
-  tasks: [],
-  selectedTasks: []
+  tasks: []
 })
 
-export const getters = {
-  getTasks(state) {
-    return state.tasks
-  },
-  getSelectedTasks(state) {
-    return state.selectedTasks
-  }
-}
-
 export const mutations = {
-  setTasks(state, tasks) {
+  SET_TASKS (state, tasks) {
     state.tasks = tasks
   },
-  setSelectedTasks(state, tasks) {
-    state.selectedTasks = tasks
+  ADD_TASK (state, task) {
+    state.tasks.unshift(task)
+  },
+  UPDATE_TASK (state, task) {
+    const index = state.tasks.findIndex(item => item.id === task.id)
+    state.tasks.splice(index, 1, task)
+  },
+  DELETE_TASK (state, taskId) {
+    const index = state.tasks.findIndex(item => item.id === taskId)
+    state.tasks.splice(index, 1)
   }
 }
 
 export const actions = {
-  async getTasks({ commit }) {
-    const response = await this.$apis.task.tasks()
-    console.log(response.data.data)
-    // console.log(response)
-    commit('setTasks', response.data.data)
-    return response
+  async getTasks ({ commit }) {
+    try {
+      const { data } = await axios.get('/task/project/1')
+      commit('SET_TASKS', data)
+    } catch (error) {
+      console.err(error)
+      throw error
+    }
   },
-  setSelectedTasks({ commit }, tasks) {
-    commit('setSelectedTasks', tasks)
+  async addTask ({ commit }, task) {
+    try {
+      const { data } = await axios.post('/tasks', task)
+      commit('ADD_TASK', data)
+    } catch (error) {
+      console.err(error)
+      throw error
+    }
+  },
+  async updateTask ({ commit }, task) {
+    try {
+      const { data } = await axios.put(`/tasks/${task.id}`, task)
+      commit('UPDATE_TASK', data)
+    } catch (error) {
+      console.err(error)
+      throw error
+    }
+  },
+  async deleteTask ({ commit }, taskId) {
+    try {
+      await axios.delete(`/tasks/${taskId}`)
+      commit('DELETE_TASK', taskId)
+    } catch (error) {
+      console.err(error)
+      throw error
+    }
   }
 }
