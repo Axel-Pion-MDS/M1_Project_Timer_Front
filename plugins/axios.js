8c9d3d7daa7551d9f7c654fe10fb136584fc4df2
elton <elton.doisy@gmail.com>
2023-01-03 14:40:05 +0100

    feat: task delete task

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,6 +2,7 @@ export default function ({ $axios }) {
   $axios.onRequest((config) => {
     config.headers = {
       'Content-Type': 'application/json',
+      'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE',
       Accept: 'application/json',
       Authorization: `Bearer ${localStorage.getItem('JWT')}`
     }
