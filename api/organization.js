e06cf603ac99b4e0ffdda48b6c25c80384db7f75
Lucas Malatchoumy <malatchoumylucas.com>
2023-01-03 15:32:29 +0100

    feat: create organization api calls

--- api/organization.js
+++ api/organization.js
@@ -2,5 +2,19 @@
 export default axios => ({
   organizations() {
     return axios.get('/organization/')
-  }
+  },
+  get_organization(id) {
+    return axios.get(`/organization/${id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('JWT')}`
+      }
+    })
+  },
+  get_users_organization(id) {
+    return axios.get(`/user-organization/users/${id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('JWT')}`
+      }
+    })
+  },
 })
