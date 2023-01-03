e5dce0f594434f5010f6c4a4fb0e46f26b7b59ab
Lucas Malatchoumy <malatchoumylucas.com>
2023-01-03 15:31:53 +0100

    feat: create organization store

---
+++ store/organization.js
@@ -0,0 +1,48 @@
+export const state = () => ({
+  organization: {},
+  user_organization_role: {}
+})
+
+export const getters = {
+  getOrganization(state) {
+    return state.organization
+  },
+  getUserOrganizationRole(state) {
+    return state.user_organization_role
+  }
+}
+
+export const mutations = {
+  setOrganization(state, organization) {
+    state.organization = organization
+  },
+  setUserOrganizationRole(state, userOrganizationRole) {
+    state.user_organization_role = userOrganizationRole
+  }
+}
+
+export const actions = {
+//   async getOrganization({ commit, dispatch }, organization) {
+//     // console.log(state.organization)
+//     const response = await this.$apis.organization.get_organization(organization.id)
+//     console.log(response)
+//   },
+  async setOrganization({ commit, rootState }, organization) {
+    const response = await this.$apis.organization.get_organization(organization.id)
+    const res = response.data.data
+    const userOrganization = await this.$apis.organization.get_users_organization(organization.id)
+    res.users = userOrganization.data.data[1].users
+    commit('setOrganization', res)
+    res.users.forEach((user) => {
+      if (user.id === rootState.user.user.id) {
+        return commit('setUserOrganizationRole', user.role)
+      }
+    })
+  },
+
+  addUserOrganization({ commit }, form) {
+    console.log(form)
+    // const response = await this.$apis.organization.get_organization(form.id)
+    return true
+  }
+}
