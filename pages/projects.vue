32912f274912a2a38bd8b31b8a7b325428f438a7
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-01-03 18:16:25 +0100

    feat: projects view

---
+++ pages/projects.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <h1>Projects</h1>
+    <TeamsSelector :callback="selectTeam" />
+    <ProjectsCards />
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProjectsPage',
+  layout: 'default',
+  async beforeMount () {
+    await this.$store.dispatch('teamsSelector/projects')
+  },
+  methods: {
+    async selectTeam() {
+      await this.$store.dispatch('teamsSelector/projects')
+    }
+  }
+}
+</script>
