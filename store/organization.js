fc86aa24cc78105fdd8b96a194a44434ad2ce401
Lucas Malatchoumy <malatchoumylucas.com>
2023-01-03 22:19:08 +0100

    feat: update organization store

--- store/organization.js
+++ store/organization.js
@@ -27,21 +27,70 @@ export const actions = {
 //     const response = await this.$apis.organization.get_organization(organization.id)
 //     console.log(response)
 //   },
-  async setOrganization({ commit, rootState }, organization) {
+  async setOrganization({ commit, state, rootState }, organization) {
     const response = await this.$apis.organization.get_organization(organization.id)
-    const res = response.data.data
-    const userOrganization = await this.$apis.organization.get_users_organization(organization.id)
-    res.users = userOrganization.data.data[1].users
-    commit('setOrganization', res)
-    res.users.forEach((user) => {
-      if (user.id === rootState.user.user.id) {
-        return commit('setUserOrganizationRole', user.role)
+    if (response.status === 200) {
+      const res = response.data.data
+      const userOrganization = await this.$apis.organization.get_users_organization(organization.id)
+      if (userOrganization.status === 200) {
+        res.users = userOrganization.data.data[1].users
+        commit('setOrganization', res)
+        res.users.forEach((user) => {
+          if (user.id === rootState.user.user.id) {
+            return commit('setUserOrganizationRole', user.role)
+          }
+        })
       }
-    })
+    }
   },
 
-  async addUserOrganization({ commit, state }, form) {
+  async addUserOrganization({ dispatch, state }, form) {
     const response = await this.$apis.organization.add_user_organization(form, state.organization.id)
-    return response
+    if (response.data.code === 201) {
+      await dispatch('snackbar/success', {
+        message: 'This user has been added',
+        timer: 4000
+      }, { root: true })
+      await dispatch('setOrganization', state.organization)
+      return true
+    } else {
+      await dispatch('snackbar/error', {
+        message: response.data.message,
+        timer: 4000
+      }, { root: true })
+    }
+  },
+  async updateOrganization({ dispatch, state }, form) {
+    form.organization = state.organization.id
+    const response = await this.$apis.organization.update_organization(form, state.organization.id)
+    if (response.data.code === 201) {
+      await dispatch('snackbar/success', {
+        message: 'This organization has been updated',
+        timer: 4000
+      }, { root: true })
+      await dispatch('setOrganization', state.organization)
+      return true
+    } else {
+      await dispatch('snackbar/error', {
+        message: response.data.message,
+        timer: 4000
+      }, { root: true })
+    }
+  },
+  async addOrganization({ dispatch }, form) {
+    const response = await this.$apis.organization.add_organization(form)
+    if (response.data.code === 201) {
+      await dispatch('snackbar/success', {
+        message: 'This organization has been created',
+        timer: 4000
+      }, { root: true })
+      await dispatch('setOrganization', response.data.data)
+      return true
+    } else {
+      await dispatch('snackbar/error', {
+        message: response.data.message,
+        timer: 4000
+      }, { root: true })
+    }
   }
 }
