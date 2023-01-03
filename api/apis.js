3c020f05ccf06fcdf66a3259837605ca71ebbb92
Lucas Malatchoumy <malatchoumylucas.com>
2023-01-03 15:36:49 +0100

    feat: add apis files

--- api/apis.js
+++ api/apis.js
@@ -2,10 +2,12 @@ import user from './user'
 import team from './team'
 import project from './project'
 import organization from './organization'
+import role from './role'
 
 export default axios => ({
   user: user(axios),
   team: team(axios),
   project: project(axios),
-  organization: organization(axios)
+  organization: organization(axios),
+  role: role(axios)
 })
