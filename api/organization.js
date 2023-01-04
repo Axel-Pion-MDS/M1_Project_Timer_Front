df2f4f5e54e28971de19811d3ddf243b5a0dee03
Lucas Malatchoumy <malatchoumylucas.com>
2023-01-04 19:27:09 +0100

    feat: add deleteUser and updateUserRole api calls

--- api/organization.js
+++ api/organization.js
@@ -21,6 +21,22 @@ export default axios => ({
     }
     return axios.post('/user-organization/add', data)
   },
+  delete_user_organization(id, email) {
+    return axios.delete('/user-organization/delete', {
+      data: {
+        user: email,
+        organization: id
+      }
+    })
+  },
+  update_user_organization_role(id, email, role) {
+    const data = {
+      user: email,
+      organization: id,
+      role: role.id
+    }
+    return axios.patch('/user-organization/update', data)
+  },
   update_organization(form) {
     return axios.patch('/organization/update', form)
   },
